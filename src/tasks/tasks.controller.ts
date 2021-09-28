3524f78a78783a63cc6d6511676afe4149df4f01
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-09-28 11:00:03 +0700

    chore: create tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -10,5 +10,12 @@ export class TasksController {
     getAllTasks(): Task[] {
         return this.taskService.getAllTasks();
     }
-    
+
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ): Task {
+        return this.taskService.createTask(title, description);
+    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './tasks.model';
-
+import { Task, TaskStatus } from './tasks.model';
+import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class TasksService {
     private tasks: Task[] = [];
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    createTask(title: string, description: string) {
+        const task: Task = {
+            id: uuidv4(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+
+        this.tasks.push(task);
+        return task;
     }
 }
