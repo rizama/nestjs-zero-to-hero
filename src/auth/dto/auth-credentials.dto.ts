3689b4eea4e2ccbf636a78cba0b0ffb919bd3376
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-10-06 23:14:38 +0700

    chore: validation AuthCredentialsDto

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,28 @@
+import {
+    IsNotEmpty,
+    IsString,
+    Matches,
+    MaxLength,
+    MinLength,
+} from 'class-validator';
+
 export class AuthCredentialsDto {
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    // Passwords will contain at least 1 upper case letter
+    // Passwords will contain at least 1 lower case letter
+    // Passwords will contain at least 1 number or special character
+    // There is no length validation (min, max) in this regex!
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'password is to weak',
+    })
     password: string;
 }
