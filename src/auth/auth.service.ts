4c2f1fbcb6496c4768ad7dc3c45c3eff6ea50b1f
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-10-06 22:53:18 +0700

    feat: signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private readonly userRepository: UserRepository
-    ){}
+        private readonly userRepository: UserRepository,
+    ) {}
+
+    signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.signUp(authCredentialsDto);
+    }
 }
