a24ee2417c5b5c33d83827053ad9d9ef9a712edf
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-10-06 12:28:49 +0700

    fix: lack entity decorator

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
-import { BaseEntity, Column, PrimaryGeneratedColumn } from "typeorm";
-import { TaskStatus } from "./tasks.model";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskStatus } from './task.enum';
 
+@Entity("tasks") // => table name. If not specified, then table name is generated from entity class name.
 export class TaskEntity extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
