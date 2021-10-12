90e5c40c4d0ae5f300d2d17b93b2f2fe12e9cf52
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-10-12 23:06:32 +0700

    chore: impl Logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 require('dotenv').config({ path: `../${process.env.NODE_ENV}.env` });
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const logger = new Logger('bootstrap');
+
+    const PORT = 3000;
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT);
+
+    logger.log(`Application Listening to port: ${PORT}`);
 }
 bootstrap();
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Logger, Param, ParseIntPipe, Patch, Post, Query, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/user.entity';
@@ -13,6 +13,7 @@ import { TasksService } from './tasks.service';
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+    private logger = new Logger('TasksController');
     constructor(private taskService: TasksService) {}
 
     @Get()
@@ -20,6 +21,7 @@ export class TasksController {
         @Query(ValidationPipe) filterDto: GetTaskFilterDto,
         @GetUser() user: User,
     ): Promise<TaskEntity[]> {
+        this.logger.verbose(`User "${user.username}" retrieving all tasks. Filters: ${JSON.stringify(filterDto)}`);
         return this.taskService.getTasks(filterDto, user);
     }
 
@@ -37,6 +39,7 @@ export class TasksController {
         @Body() createTaskDto: CreateTaskDto,
         @GetUser() user: User,
     ): Promise<TaskEntity> {
+        this.logger.verbose(`User "${user.username}" creating a new task. Data: ${JSON.stringify(createTaskDto)}`);
         return this.taskService.createTask(createTaskDto, user);
     }
 
